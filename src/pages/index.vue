1b62be4e8947f233b619fabc792b29d79e0a6cb5
Moretti Tony <tony.moretti@edu.univ-fcomte.fr>
2024-05-17 17:03:10 +0200

    Création des cards et  liaison de la base de données aux cards

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,18 @@
 <script setup lang="ts">
-//
+import type { MaisonRecord } from '@/pocketbase-types'
+import MaisonCard from '@/components/MaisonCard.vue'
+import { pb } from '@/backend'
+
+const maisonsListe = await pb.collection('maison').getFullList()
+
 </script>
 
 <template>
-  <h1 class="text-2xl">Bonjour monde !</h1>
+  <h1 class="text-2xl ">Bonjour monde !</h1>
+  <MaisonCard v-for="maison in maisonsListe" v-bind="maison" />
+  <!-- <MaisonCard v-bind="maisonsListe[0]" /> -->
+  <!-- <MaisonCard v-bind="maisonsListe[1]" />
+  <MaisonCard v-bind="maisonsListe[2]" />
+  <MaisonCard v-bind="maisonsListe[3]" />
+  <MaisonCard v-bind="maisonsListe[4]" /> -->
 </template>
